ced51f5250eb13a44ee6947f66205f2ac3bc5efa
Subbarao Garlapati <sgarlapati9@gmail.com>
2022-03-08 21:24:26 -0500

    added P3 tests

--- project2_files/de_stage.v
+++ project2_files/de_stage.v
@@ -282,7 +282,7 @@ assign regval2_DE = regs[rs2_DE];
                     || (op_I_DE == `SUB_I) || (op_I_DE == `MUL_I) || (op_I_DE == `AND_I)
                     || (op_I_DE == `ANDI_I) || (op_I_DE == `OR_I) || (op_I_DE == `ORI_I)
                     || (op_I_DE == `XOR_I) || (op_I_DE == `XORI_I) || (op_I_DE == `SLT_I)
-                    || (op_I_DE == `SLTI_I) || (op_DE == `SLTU_I) || (op_I_DE == `SLTIU_I)
+                    || (op_I_DE == `SLTI_I) || (op_I_DE == `SLTU_I) || (op_I_DE == `SLTIU_I)
                     || (op_I_DE == `SRA_I) || (op_I_DE == `SRAI_I) || (op_I_DE == `SRL_I)
                     || (op_I_DE == `SRLI_I) || (op_I_DE == `SLL_I) || (op_I_DE == `SLLI_I)
                     || (op_I_DE == `LW_I)) ?  1: 0 ; 
